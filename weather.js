3f6859802b230054c183866c357c3051790c4870
Rajdeep <rajdeepadhikary@protonmail.com>
2022-06-30 19:53:42 +0530

    adding files.

---
+++ weather.js
@@ -0,0 +1,8 @@
+const weather = require('weather-js');
+
+
+weather.find({search: 'Howrah, IN', degreeType: 'C'}, function(err, result) {
+    if(err) console.log(err);
+
+    console.log(JSON.stringify(result, null, 2));
+});
